d571e206a4261efe33ee127d2c177719dcd945c9
Odhiambo254 <odhiamboandrew76@gmail.com>
2023-10-06 17:35:40 +0000

    lowercaseuppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  *main - entry point of program
+  *
+  *Return:always 0
+  */
+int main(void)
+{
+	char = c;
+
+	while (c <= 'z')
+	{
+	putchar(c);
+	c++;
+	}
+	else if (c != q && != e)
+		putchar(c);
+		c++;
+		{
+			putchar('\n');
+			return (0);
+		}
+}
