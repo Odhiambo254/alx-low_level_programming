c942dc920805d410d6d0d51ce176198e4ef22149
Odhiambo254 <odhiamboandrew76@gmail.com>
2023-10-06 16:36:54 +0000

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main- entry point
+  *
+  *Return:always 0
+  */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter <= 'z')
+
+	putchar(letter);
+	letter++;
+	{
+		putchar(letter);
+		return (0);
+	}
+}
