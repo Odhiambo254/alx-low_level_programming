1944269de74f4cd13556ffe118819a392478e058
Odhiambo254 <odhiamboandrew76@gmail.com>
2023-10-06 18:33:32 +0000

    except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main-entry point
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	if (letter != 'q' && letter != 'e')
+	putchar(letter);
+	letter++;
+	}
+	putchar('\n');
+	return (0);
+}
