3655d5307a3443d02d1938dae12ce275552ead90
Odhiambo254 <odhiamboandrew76@gmail.com>
2023-10-11 17:35:28 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main-entry point
+  *
+  *Return:always -0
+  */
+int main(void)
+{
+	int n;
+	char p[] = "_putchar";
+
+	for (n = 0; n <= 8; n++)
+	{
+	putchar(p[n]);
+	}
+	putchar('\n');
+	return (0);
+}
