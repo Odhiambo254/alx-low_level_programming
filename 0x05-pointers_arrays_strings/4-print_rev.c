b8223d88c817b3771045713637c6f5cd66a5ceb7
Odhiambo254 <odhiamboandrew76@gmail.com>
2024-03-10 11:32:30 +0300

    print string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *print_rev - print the string in reverse order followed by a new line
+  *@s: string to be printed
+  *return: 0
+  */
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (int i = length - 1; i >= 0; i--)
+	{
+		_putchar('\n');
+	}
+}
